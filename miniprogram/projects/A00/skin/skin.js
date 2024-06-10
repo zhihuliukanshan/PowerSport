806e89aa860f4f45ec7bc57a6e8e9cc9cf1c9b7e
zhihuliukanshan@outlook.com <zhihuliukanshan@outlook.com>
2024-06-10 10:17:05 +0800

    CR

---
+++ miniprogram/projects/A00/skin/skin.js
@@ -0,0 +1,43 @@
+module.exports = {
+	PID: 'A00', //健身馆
+
+	NAV_COLOR: '#ffffff',
+	NAV_BG: '#131313',
+
+	MEET_NAME: '预约', 
+ 
+	MENU_ITEM: ['首页', '预约日历', '我的'], // 第1,4,5菜单
+
+	NEWS_CATE: '1=小黑板,2=健身常识',
+	MEET_TYPE: '1=教练预约|leftbig2,2=项目预约|leftbig3',
+
+	DEFAULT_FORMS: [{
+			type: 'line',
+			title: '姓名',
+			desc: '请填写您的姓名',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		},
+		{
+			type: 'line',
+			title: '手机',
+			desc: '请填写您的手机号码',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		}
+	]
+}
